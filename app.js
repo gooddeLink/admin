236a28ec430eb4a1747eba91e4b08267d18cba69
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2022-09-27 18:42:36 +0900

    call_cam websocket

--- app.js
+++ app.js
@@ -76,20 +76,19 @@ server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
-const WebSocket = require('ws');
+// const WebSocket = require('ws');
 
-const socket = new WebSocket.Server({
-  port : 5050
-})
-
-socket.on('connection', (ws, req)=>{
-  ws.on('message',(msg)=>{
-    console.log('유저가 보낸 거 : '+ msg);
-  })
-  ws.interval = setInterval(()=>{
-    if(ws.readyState!=ws.OPEN){
-      return;
-    }
-    ws.send("서버에서 클라이언트로 메시지를 보냅니다.");
-  },3000);
-})
+// const socket = new WebSocket.Server({
+//   port : 5050
+// })
+// socket.on('connection', (ws, req)=>{
+//   ws.on('message',(msg)=>{
+//     console.log('유저가 보낸 거 : '+ msg);
+//   })
+//   ws.interval = setInterval(()=>{
+//     if(ws.readyState!=ws.OPEN){
+//       return;
+//     }
+//     // ws.send("서버에서 클라이언트로 메시지를 보냅니다.");
+//   },3000);
+// // })
